b121f8188c2a1020e0e4c9a40b1ad9fad54fbc8d
mutahharbm <mutahharbm1998@gmail.com>
2022-04-04 13:57:11 +0500

    Added sliderCarousel component

---
+++ src/components/styled.components.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  border: 1px solid red;
+  padding: 90px 0;
+`;
+
+export const Container = styled.div`
+  padding: 0 0.75rem;
+`;
+
+export const Slide = styled.div``;
